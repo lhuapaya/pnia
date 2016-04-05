7b894f6f95deecb00c272a67af27ebee24b7a6a3
lhuapaya <ing.huapaya@gmail.com>
2016-04-05 10:18:47 -0500

    a

--- views/proyecto/nuevo.php
+++ views/proyecto/nuevo.php
@@ -219,13 +219,8 @@ use app\models\Maestros;
                 <textarea type="text"  placeholder="..."  rows="10" cols="80" style="margin: 0px; width: 600px; height: 80px;" id="proyecto-objetivo_general" name="Proyecto[objetivo_general]"  required><?= $proyecto->objetivo_general?></textarea>
             </li>
             <li>
-                <h4>1.10 Objeto General</h4>
-                <label for="proyecto-objetivo_general">Señale Objeto General:</label>
-                <textarea type="text"  placeholder="..."  rows="10" cols="80" style="margin: 0px; width: 600px; height: 80px;" id="proyecto-objetivo_general" name="Proyecto[objetivo_general]"  required><?= $proyecto->objetivo_general?></textarea>
-                <h5>Señale los Objetos Especificos:<h5>
-                <?= \app\widgets\objetivosespecificos\ObjetivosEspecificosWidget::widget(['proyecto_id'=>$proyecto->id]); ?> <br>
-                <?= \app\widgets\indicadores\IndicadoresWidget::widget(['proyecto_id'=>$proyecto->id]); ?> 
-
+                <h5>3.1.2 Objetos Especificos</h5>
+                <?= \app\widgets\objetivosespecificos\ObjetivosEspecificosWidget::widget(['proyecto_id'=>$proyecto->id]); ?> 
             </li>
             <li>
                 <h4>3.2 Indicadores</h4>
