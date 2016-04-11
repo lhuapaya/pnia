de04116260fde5502a737600051084332ce8aa7c
lhuapaya <ing.huapaya@gmail.com>
2016-04-11 08:48:05 -0500

    xinin

--- controllers/UsuariosController.php
+++ controllers/UsuariosController.php
@@ -5,12 +5,10 @@ namespace app\controllers;
 use Yii;
 use app\models\Usuarios;
 use app\models\UsuariosSearch;
-<<<<<<< HEAD
-=======
+
 use app\models\Perfil;
 use app\models\AuthAssignment;
 
->>>>>>> 6a4012ce8b432cf3dee830211093ae03c6094858
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -86,32 +84,7 @@ class UsuariosController extends Controller
      */
     public function actionUpdate($id)
     {
-<<<<<<< HEAD
-        $model = $this->findModel($id);
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-=======
-        $this->layout='principal';
-        $model = $this->findModel($id);
-        $perfiles=Perfil::find()->all();
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            $assigment=AuthAssignment::find()->where("user_id='".$model->id."'")->one();
-            $assigment->user_id="$model->id";
-            $assigment->item_name="$model->id_perfil";
-            $assigment->update();
-            
-            return $this->redirect(['index', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-                'perfiles'=>$perfiles
->>>>>>> 6a4012ce8b432cf3dee830211093ae03c6094858
-            ]);
-        }
     }
 
     /**
@@ -122,12 +95,7 @@ class UsuariosController extends Controller
      */
     public function actionDelete($id)
     {
-        $this->findModel($id)->delete();
-<<<<<<< HEAD
-
-=======
         AuthAssignment::find()->where("user_id='".$id."'")->one()->delete();
->>>>>>> 6a4012ce8b432cf3dee830211093ae03c6094858
         return $this->redirect(['index']);
     }
 
@@ -146,8 +114,7 @@ class UsuariosController extends Controller
             throw new NotFoundHttpException('The requested page does not exist.');
         }
     }
-<<<<<<< HEAD
-=======
+
     
     public function actionNuevo()
     {
@@ -170,5 +137,5 @@ class UsuariosController extends Controller
             ]);
         }
     }
->>>>>>> 6a4012ce8b432cf3dee830211093ae03c6094858
+
 }
